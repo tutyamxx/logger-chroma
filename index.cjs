1f6aad6750c21bc3a163946c5503024e643f4297
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-12 18:34:29 +0000

    UPDATE: Workflow update

--- index.cjs
+++ index.cjs
@@ -1,6 +1,6 @@
 /**
  *  logger-chroma - 🦄 A colorful, developer-friendly Node.js logger with timestamps, emojis, pretty-printed objects, and grouped logs for clear, readable output.
- *  @version: v1.0.7
+ *  @version: v1.0.8
  *  @link: https://github.com/tutyamxx/logger-chroma
  *  @license: MIT
  **/
